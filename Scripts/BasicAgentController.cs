cd48d9be38e4aaf490e24fcb49a33f1a22346314
codecoach-joshua <42500661+codecoach-joshua@users.noreply.github.com>
2019-12-15 15:45:23 -0800

    Added missing libraries to BasicAgentController.cs

--- Scripts/BasicAgentController.cs
+++ Scripts/BasicAgentController.cs
@@ -1,28 +1,33 @@
 using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
-public class BasicAgentController : MonoBehaviour {
+public class BasicAgentController : MonoBehaviour
+{
 
-  //reference for the NavMeshAgent component used by this object
-  private NavMeshAgent navAgent;
-  
-  //reference to the game object for the main player
-  private GameObject player;
+    //reference for the NavMeshAgent component used by this object
+    private NavMeshAgent navAgent;
 
-	// Use this for initialization
-	void Start () {
-    //load the component for the NavMeshAgent
-    navAgent = GetComponent<NavMeshAgent>();
-    
-    //load game object for the main player by finding it based on the "Player" tag
-	  player = GameObject.FindGameObjectWithTag("Player");
-	}
-	
-	// Update is called once per frame
-	void Update () {
-    // NavMeshAgent will automatically control its game object to move to its "destination"
-    
-    // have basic navAgent follow and chase the player
-	  navAgent.destination = player.transform.position;      
-	}
+    //reference to the game object for the main player
+    private GameObject player;
+
+    // Use this for initialization
+    void Start()
+    {
+        //load the component for the NavMeshAgent
+        navAgent = GetComponent<NavMeshAgent>();
+
+        //load game object for the main player by finding it based on the "Player" tag
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // NavMeshAgent will automatically control its game object to move to its "destination"
+
+        // have basic navAgent follow and chase the player
+        navAgent.destination = player.transform.position;
+    }
 }
